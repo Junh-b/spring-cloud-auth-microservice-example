bb978161d13ef84c39e35005fa8d7cf590808da9
Baek Junha <jooha208@gmail.com>
2021-06-29 11:20:15 +0900

    initial project structure

---
+++ src/main/java/net/junhabaek/authservice/repository/README.md
@@ -0,0 +1,1 @@
+dummy markdown for building initial project structure
---
+++ src/main/java/net/junhabaek/authservice/web/rest/HealthCheckController.java
@@ -0,0 +1,15 @@
+package net.junhabaek.authservice.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "ok";
+    }
+}
