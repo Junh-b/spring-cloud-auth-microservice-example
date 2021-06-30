14be5a240b93816a9168a75b376bdc22689c68eb
Baek Junha <jooha208@gmail.com>
2021-06-30 16:49:01 +0900

    add validation in user controller

--- src/main/java/net/junhabaek/authservice/web/rest/UserController.java
+++ src/main/java/net/junhabaek/authservice/web/rest/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody CreateMemberRequest cmr){
+    public ResponseEntity<String> createUser(@RequestBody @Valid CreateMemberRequest cmr){
         User user = userService.createUser(cmr);
         return ResponseEntity.ok(user.getId().toString());
     }
