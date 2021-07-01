b2df1452f97b7af8fc95e964c21aa634bc0d3e9a
Baek Junha <jooha208@gmail.com>
2021-07-02 00:24:04 +0900

    apply transaction to user service

--- src/main/java/net/junhabaek/authservice/service/UserServiceImpl.java
+++ src/main/java/net/junhabaek/authservice/service/UserServiceImpl.java
@@ -7,9 +7,11 @@ import net.junhabaek.authservice.domain.User;
 import net.junhabaek.authservice.repository.UserRepository;
 import net.junhabaek.authservice.service.dto.CreateUserRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
     private final CreateUserService createUserService;
@@ -23,11 +25,13 @@ public class UserServiceImpl implements UserService{
         return user;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User findUserByUserId(Long userId) {
         return userRepository.findById(userId).orElseThrow(()->new EntityNotFoundException("entity not found"));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Iterable<User> findAllUser() {
         return userRepository.findAll();
