85832083f06a014ef4da2952db97a8a2aed9145b
Baek Junha <jooha208@gmail.com>
2021-08-06 14:21:50 +0900

    apply spring config

--- src/main/java/net/junhabaek/authservice/web/rest/HealthCheckController.java
+++ src/main/java/net/junhabaek/authservice/web/rest/HealthCheckController.java
@@ -1,5 +1,7 @@
 package net.junhabaek.authservice.web.rest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HealthCheckController {
+    private final Environment env;
 
     @GetMapping("/health_check")
     public ResponseEntity<String> status(){
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(String.format(env.getProperty("token.secret")));
     }
 }
